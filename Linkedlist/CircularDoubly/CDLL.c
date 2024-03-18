64a7a699396d4ecf4df5d8de09cdd0cd5156882d
Thejas <teju270934@gmail.com>
2024-03-18 12:36:56 +0530

    indent made proper

--- Linkedlist/CircularDoubly/CDLL.c
+++ Linkedlist/CircularDoubly/CDLL.c
@@ -2,246 +2,257 @@
 #include <stdlib.h>
 struct Node
 {
-  struct Node *prev;
-  int data;
-  struct Node *next;
+    struct Node *prev;
+    int data;
+    struct Node *next;
 };
+
 struct CDLL
 {
-  struct Node *head;
-  int count;
+    struct Node *head;
+    int count;
 };
-struct Node *
-createNode(int value)
+
+struct Node *createNode(int value)
 {
-  struct Node *newnode = (struct Node *)malloc(sizeof(struct Node));
-  if (newnode == NULL)
-  {
-    printf("Memory Allocation Failed...\n");
-    exit(1);
-  }
-  newnode->data = value;
-  newnode->prev = newnode->next = newnode;
-  return newnode;
+    struct Node *newnode = (struct Node *)malloc(sizeof(struct Node));
+    if (newnode == NULL)
+    {
+        printf("Memory Allocation Failed...\n");
+        exit(1);
+    }
+    newnode->data = value;
+    newnode->prev = newnode->next = newnode;
+    return newnode;
 }
+
 void createList(struct CDLL *mylist)
 {
-  mylist->head = createNode(0);
-  mylist->head->next = mylist->head->prev = NULL;
-  mylist->count = 0;
+    mylist->head = createNode(0);
+    mylist->head->next = mylist->head->prev = NULL;
+    mylist->count = 0;
 }
+
 void display(struct CDLL *list)
 {
-  struct Node *current = list->head->next;
-  do
-  {
-    printf("%d ", current->data);
-    current = current->next;
-  } while (current != list->head->next);
-  printf("\n");
+    struct Node *current = list->head->next;
+    do
+    {
+        printf("%d ", current->data);
+        current = current->next;
+    } while (current != list->head->next);
+    printf("\n");
 }
+
 void insertAtStart(struct CDLL *list, int value)
 {
-  struct Node *newnode = createNode(value);
-  struct Node *current = list->head->next;
-  if (current == NULL)
-  {
-    // If the list is empty
-    list->head->next = newnode;
-  }
-  else
-  {
-    // Insert at the start
-    newnode->next = current;
-    newnode->prev = current->prev;
-    current->prev->next = newnode;
-    current->prev = newnode;
-  }
-  list->count++;
-}
-void insertAtEnd(struct CDLL *list, int value)
-{
-  struct Node *newnode = createNode(value);
-  if (list->head->next == NULL)
-  {
-    // If the list is empty
-    list->head->next = newnode;
-  }
-  else
-  {
-    list->head->next->prev->next = newnode;
-    newnode->prev = list->head->next->prev;
-    newnode->next = list->head->next;
-    list->head->next->prev = newnode;
-  }
-  list->count++;
-}
-void insertAtpos(struct CDLL *list, int value, int pos)
-{
-  if (pos < 0 || pos > list->count)
-  {
-    printf("Invalid position. Position is out of range\n");
-  }
-  else if (pos == 0)
-  {
-    insertAtStart(list, value);
-  }
-  else if (pos == list->count)
-  {
-    insertAtEnd(list, value);
-  }
-  else
-  {
     struct Node *newnode = createNode(value);
     struct Node *current = list->head->next;
-    for (int i = 0; i < pos - 1; i++)
+    if (current == NULL)
     {
-      current = current->next;
+        // If the list is empty
+        list->head->next = newnode;
     }
-    newnode->next = current->next;
-    newnode->prev = current;
-    current->next->prev = newnode;
-    current->next = newnode;
+    else
+    {
+        // Insert at the start
+        newnode->next = current;
+        newnode->prev = current->prev;
+        current->prev->next = newnode;
+        current->prev = newnode;
+    }
     list->count++;
-  }
 }
+
+void insertAtEnd(struct CDLL *list, int value)
+{
+    struct Node *newnode = createNode(value);
+    if (list->head->next == NULL)
+    {
+        // If the list is empty
+        list->head->next = newnode;
+    }
+    else
+    {
+        list->head->next->prev->next = newnode;
+        newnode->prev = list->head->next->prev;
+        newnode->next = list->head->next;
+        list->head->next->prev = newnode;
+    }
+    list->count++;
+}
+
+void insertAtpos(struct CDLL *list, int value, int pos)
+{
+    if (pos < 0 || pos > list->count)
+    {
+        printf("Invalid position. Position is out of range\n");
+    }
+    else if (pos == 0)
+    {
+        insertAtStart(list, value);
+    }
+    else if (pos == list->count)
+    {
+        insertAtEnd(list, value);
+    }
+    else
+    {
+        struct Node *newnode = createNode(value);
+        struct Node *current = list->head->next;
+        for (int i = 0; i < pos - 1; i++)
+        {
+            current = current->next;
+        }
+        newnode->next = current->next;
+        newnode->prev = current;
+        current->next->prev = newnode;
+        current->next = newnode;
+        list->count++;
+    }
+}
+
 int deleteAtStart(struct CDLL *list)
 {
-  int x = -1;
-  struct Node *current = list->head->next;
-  struct Node *last = list->head->next->prev;
-  if (list->head->next == NULL)
-  {
-    printf("List is empty\n");
-    return -1;
-  }
-  current->next->prev = last;
-  last->next = current->next;
-  list->head->next = current->next;
-  x = current->data;
-  free(current);
-  list->count--;
-  return x;
-}
-int deleteAtEnd(struct CDLL *list)
-{
-  int x = -1;
-  struct Node *last = list->head->next;
-  if (list->head->next == NULL)
-  {
-    printf("list is empty\n");
-    return -1;
-  }
-  while (last->next != list->head->next)
-  {
-    last = last->next;
-  }
-  last->prev->next = list->head->next;
-  list->head->prev = last->prev;
-  x = last->data;
-  free(last);
-  list->count--;
-  return x;
-}
-int deleteAtPos(struct CDLL *list, int pos)
-{
-  if (pos < 0 || pos > list->count)
-  {
-    printf("\ninvalid position. cannot delete the element\n");
-    return -1;
-  }
-  else if (pos == 0)
-  {
-    deleteAtStart(list);
-  }
-  else if (pos == list->count)
-  {
-    deleteAtEnd(list);
-  }
-  else
-  {
+    int x = -1;
     struct Node *current = list->head->next;
-    struct Node *prev = NULL;
-    for (int i = 0; i < pos && current->next != list->head->next; i++)
+    struct Node *last = list->head->next->prev;
+    if (list->head->next == NULL)
     {
-      prev = current;
-      current = current->next;
+        printf("List is empty\n");
+        return -1;
     }
-    current->next->prev = current->prev;
-    prev->next = current->next;
-    int x = current->data;
+    current->next->prev = last;
+    last->next = current->next;
+    list->head->next = current->next;
+    x = current->data;
     free(current);
     list->count--;
     return x;
-  }
 }
+
+int deleteAtEnd(struct CDLL *list)
+{
+    int x = -1;
+    struct Node *last = list->head->next;
+    if (list->head->next == NULL)
+    {
+        printf("list is empty\n");
+        return -1;
+    }
+    while (last->next != list->head->next)
+    {
+        last = last->next;
+    }
+    last->prev->next = list->head->next;
+    list->head->prev = last->prev;
+    x = last->data;
+    free(last);
+    list->count--;
+    return x;
+}
+
+int deleteAtPos(struct CDLL *list, int pos)
+{
+    if (pos < 0 || pos > list->count)
+    {
+        printf("\ninvalid position. cannot delete the element\n");
+        return -1;
+    }
+    else if (pos == 0)
+    {
+        deleteAtStart(list);
+    }
+    else if (pos == list->count)
+    {
+        deleteAtEnd(list);
+    }
+    else
+    {
+        struct Node *current = list->head->next;
+        struct Node *prev = NULL;
+        for (int i = 0; i < pos && current->next != list->head->next; i++)
+        {
+            prev = current;
+            current = current->next;
+        }
+        current->next->prev = current->prev;
+        prev->next = current->next;
+        int x = current->data;
+        free(current);
+        list->count--;
+        return x;
+    }
+}
+
 int searchByKey(struct CDLL *list, int key)
 {
-  struct Node *current = list->head->next;
-  int pos = 0;
-  if (list->head->next == NULL)
-  {
-    printf("\nlist is empty\n");
+    struct Node *current = list->head->next;
+    int pos = 0;
+    if (list->head->next == NULL)
+    {
+        printf("\nlist is empty\n");
+        return -1;
+    }
+    do
+    {
+        if (key == current->data)
+            return pos;
+        pos++;
+        current = current->next;
+    } while (current != list->head->next);
+    printf("Key not found\n");
     return -1;
-  }
-  do
-  {
-    if (key == current->data)
-      return pos;
-    pos++;
-    current = current->next;
-  } while (current != list->head->next);
-  printf("Key not found\n");
-  return -1;
 }
+
 int deleteByKey(struct CDLL *list, int key)
 {
-  int pos = searchByKey(list, key);
-  if (pos == -1)
-  {
-    printf("\nelement cannot be deleted\n");
-    return -1;
-  }
-  else
-  {
-    deleteAtPos(list, pos);
-  }
+    int pos = searchByKey(list, key);
+    if (pos == -1)
+    {
+        printf("\nelement cannot be deleted\n");
+        return -1;
+    }
+    else
+    {
+        deleteAtPos(list, pos);
+    }
 }
 
 void insertInOrder(struct CDLL *list, int value)
 {
-  struct Node *newnode = createNode(value);
-  if (list->head == NULL || value < list->head->next->data)
-  {
-    insertAtStart(list, value);
-  }
+    struct Node *newnode = createNode(value);
+    if (list->head == NULL || value < list->head->next->data)
+    {
+        insertAtStart(list, value);
+    }
 
-  struct Node *current = list->head->next;
-  struct Node *prev = NULL;
-  while (value > current->data && current->next != list->head->next)
-  {
-    prev = current;
-    current = current->next;
-  }
-  if (value > current->data)
-  {
-    insertAtEnd(list, value);
-  }
-  else
-  {
-    prev->next = newnode;
-    newnode->next = current;
-    list->count++;
-  }
+    struct Node *current = list->head->next;
+    struct Node *prev = NULL;
+    while (value > current->data && current->next != list->head->next)
+    {
+        prev = current;
+        current = current->next;
+    }
+    if (value > current->data)
+    {
+        insertAtEnd(list, value);
+    }
+    else
+    {
+        prev->next = newnode;
+        newnode->next = current;
+        list->count++;
+    }
 }
 
 int main()
 {
-  struct CDLL *list = (struct CDLL *)malloc(sizeof(struct CDLL));
-  createList(list);
-  insertAtEnd(list, 90);
-  insertAtEnd(list, 20);
-  insertInOrder(list, 55);
-  display(list);
-  return 0;
+    struct CDLL *list = (struct CDLL *)malloc(sizeof(struct CDLL));
+    createList(list);
+    insertAtEnd(list, 90);
+    insertAtEnd(list, 20);
+    insertInOrder(list, 55);
+    display(list);
+    return 0;
 }
--- Linkedlist/CircularLL/CLL.c
+++ Linkedlist/CircularLL/CLL.c
@@ -3,338 +3,337 @@
 
 struct Node
 {
-  int data;
-  struct Node *next;
+      int data;
+      struct Node *next;
 };
 
 struct CLL
 {
-  struct Node *head;
-  int count;
+      struct Node *head;
+      int count;
 };
 
 struct Node *createNode(int value)
 {
-  struct Node *newnode = (struct Node *)malloc(sizeof(struct Node));
-  if (newnode == NULL)
-  {
-    printf("memory allocation failed..\n");
-    exit(1);
-  }
-  newnode->data = value;
-  newnode->next = NULL;
+      struct Node *newnode = (struct Node *)malloc(sizeof(struct Node));
+      if (newnode == NULL)
+      {
+            printf("memory allocation failed..\n");
+            exit(1);
+      }
+      newnode->data = value;
+      newnode->next = NULL;
 
-  return newnode;
+      return newnode;
 }
 
 void createList(struct CLL *list, int value)
 {
-  list->head = createNode(value);
+      list->head = createNode(value);
 }
 
 void insertAtEnd(struct CLL *list, int value)
 {
-  struct Node *newnode = createNode(value);
-  struct Node *current = list->head->next;
-  if (list->head->next == NULL)
-  {
-    list->head->next = newnode;
-    newnode->next = newnode;
-    list->count++;
-    return;
-  }
-  while (current->next != list->head->next)
-  {
-    current = current->next;
-  }
-  newnode->next = current->next;
-  current->next = newnode;
-  list->count++;
+      struct Node *newnode = createNode(value);
+      struct Node *current = list->head->next;
+      if (list->head->next == NULL)
+      {
+            list->head->next = newnode;
+            newnode->next = newnode;
+            list->count++;
+            return;
+      }
+      while (current->next != list->head->next)
+      {
+            current = current->next;
+      }
+      newnode->next = current->next;
+      current->next = newnode;
+      list->count++;
 }
 
 void insertAtStart(struct CLL *list, int value)
 {
-  struct Node *newnode = createNode(value);
-  struct Node *current = list->head->next;
-  if (list->head->next == NULL)
-  {
-    list->head->next = newnode;
-    newnode->next = newnode;
-    list->count++;
-    return;
-  }
-  while (current->next != list->head->next)
-    current = current->next;
-  current->next = newnode;
-  newnode->next = list->head->next;
-  list->head->next = newnode;
-  list->count++;
+      struct Node *newnode = createNode(value);
+      struct Node *current = list->head->next;
+      if (list->head->next == NULL)
+      {
+            list->head->next = newnode;
+            newnode->next = newnode;
+            list->count++;
+            return;
+      }
+      while (current->next != list->head->next)
+            current = current->next;
+      current->next = newnode;
+      newnode->next = list->head->next;
+      list->head->next = newnode;
+      list->count++;
 }
 
 void insertAtPos(struct CLL *list, int value, int pos)
 {
-  struct Node *current = list->head->next;
-  if (pos < 0 || pos > list->count - 1)
-  {
-    printf("Invalid position. element cannot be inserted\n");
-    return;
-  }
-  else if (pos == 0)
-  {
-    insertAtStart(list, value);
-  }
-  else if (pos < list->count - 1)
-  {
-    insertAtEnd(list, value);
-  }
-  else
-  {
-    struct Node *newnode = createNode(value);
-    for (int i = 0; i < pos - 1 && current->next != list->head->next; i++)
-    {
-      current = current->next;
-    }
-    if (current->next == list->head->next)
-    {
-      printf("invalid position to insert the element\n");
-      return;
-    }
-    newnode->next = current->next;
-    current->next = newnode;
-  }
+      struct Node *current = list->head->next;
+      if (pos < 0 || pos > list->count - 1)
+      {
+            printf("Invalid position. element cannot be inserted\n");
+            return;
+      }
+      else if (pos == 0)
+      {
+            insertAtStart(list, value);
+      }
+      else if (pos < list->count - 1)
+      {
+            insertAtEnd(list, value);
+      }
+      else
+      {
+            struct Node *newnode = createNode(value);
+            for (int i = 0; i < pos - 1 && current->next != list->head->next; i++)
+            {
+                  current = current->next;
+            }
+            if (current->next == list->head->next)
+            {
+                  printf("invalid position to insert the element\n");
+                  return;
+            }
+            newnode->next = current->next;
+            current->next = newnode;
+      }
 }
 
 void display(struct CLL *list)
 {
-  struct Node *current = list->head->next;
-  do
-  {
-    printf("%d ", current->data);
-    current = current->next;
-  } while (current != list->head->next);
+      struct Node *current = list->head->next;
+      do
+      {
+            printf("%d ", current->data);
+            current = current->next;
+      } while (current != list->head->next);
 }
 
 int deleteAtStart(struct CLL *list)
 {
-  int x = -1;
-  struct Node *current = list->head->next;
-  struct Node *tail = list->head->next;
-  if (list->head->next == NULL)
-  {
-    printf("No element to delete\n");
-    return -1;
-  }
-  while (tail->next != list->head->next)
-  {
-    tail = tail->next;
-  }
-  tail->next = current->next;
-  list->head->next = current->next;
-  x = current->data;
-  free(current);
-  list->count--;
-  return x;
+      int x = -1;
+      struct Node *current = list->head->next;
+      struct Node *tail = list->head->next;
+      if (list->head->next == NULL)
+      {
+            printf("No element to delete\n");
+            return -1;
+      }
+      while (tail->next != list->head->next)
+      {
+            tail = tail->next;
+      }
+      tail->next = current->next;
+      list->head->next = current->next;
+      x = current->data;
+      free(current);
+      list->count--;
+      return x;
 }
 
 int deleteAtEnd(struct CLL *list)
 {
-  int x = -1;
-  struct Node *current = NULL;
-  struct Node *tail = list->head->next;
-  if (list->head->next == NULL)
-  {
-    printf("No element to delete \n");
-    return -1;
-  }
-  while (tail->next != list->head->next)
-  {
-    current = tail;
-    tail = tail->next;
-  }
-  current->next = tail->next;
-  x = tail->data;
-  free(tail);
-  list->count--;
-  return x;
+      int x = -1;
+      struct Node *current = NULL;
+      struct Node *tail = list->head->next;
+      if (list->head->next == NULL)
+      {
+            printf("No element to delete \n");
+            return -1;
+      }
+      while (tail->next != list->head->next)
+      {
+            current = tail;
+            tail = tail->next;
+      }
+      current->next = tail->next;
+      x = tail->data;
+      free(tail);
+      list->count--;
+      return x;
 }
 
 int deleteAtPos(struct CLL *list, int pos)
 {
-  struct Node *current = list->head->next;
-  struct Node *prev = NULL;
-  int x = -1;
-  if (pos < 0 || pos >= list->count)
-  {
-    printf("invalid position to delete the element\n");
-    return -1;
-  }
-  if (pos == 0)
-  {
-    x = deleteAtStart(list);
-  }
-  else if (pos == list->count - 1)
-  {
-    x = deleteAtEnd(list);
-  }
-  else
-  {
+      struct Node *current = list->head->next;
+      struct Node *prev = NULL;
+      int x = -1;
+      if (pos < 0 || pos >= list->count)
+      {
+            printf("invalid position to delete the element\n");
+            return -1;
+      }
+      if (pos == 0)
+      {
+            x = deleteAtStart(list);
+      }
+      else if (pos == list->count - 1)
+      {
+            x = deleteAtEnd(list);
+      }
+      else
+      {
 
-    for (int i = 0; i < pos && current->next != list->head->next; i++)
-    {
-      prev = current;
-      current = current->next;
-    }
-    prev->next = current->next;
-    x = current->data;
-    free(current);
-    list->count--;
-    return x;
-  }
+            for (int i = 0; i < pos && current->next != list->head->next; i++)
+            {
+                  prev = current;
+                  current = current->next;
+            }
+            prev->next = current->next;
+            x = current->data;
+            free(current);
+            list->count--;
+            return x;
+      }
 }
 
 int searchByKey(struct CLL *list, int key)
 {
-  if (list->head->next == NULL)
-  {
-    printf("List is Empty\n");
-    return -1;
-  }
-  struct Node *current = list->head->next;
-  int pos = 0;
-  do
-  {
-    if (key == current->data)
-    {
-      return pos;
-    }
-    pos++;
-    current = current->next;
-  } while (current != list->head->next);
-  printf("\nkey not found\n");
-  return -1; // indicates key not found
+      if (list->head->next == NULL)
+      {
+            printf("List is Empty\n");
+            return -1;
+      }
+      struct Node *current = list->head->next;
+      int pos = 0;
+      do
+      {
+            if (key == current->data)
+            {
+                  return pos;
+            }
+            pos++;
+            current = current->next;
+      } while (current != list->head->next);
+      printf("\nkey not found\n");
+      return -1; // indicates key not found
 }
 
 int searchByPos(struct CLL *list, int pos)
 {
-  if (list->head->next == NULL)
-  {
-    printf("List is empty\n");
-    return -1;
-  }
-  struct Node *current = list->head->next;
-  int curpos = 0;
-  do
-  {
-    if (curpos == pos)
-    {
-      return current->data;
-    }
-    curpos++;
-    current = current->next;
-  } while (current != list->head->next);
+      if (list->head->next == NULL)
+      {
+            printf("List is empty\n");
+            return -1;
+      }
+      struct Node *current = list->head->next;
+      int curpos = 0;
+      do
+      {
+            if (curpos == pos)
+            {
+                  return current->data;
+            }
+            curpos++;
+            current = current->next;
+      } while (current != list->head->next);
 
-  printf("invalid position \n");
-  return -1;
+      printf("invalid position \n");
+      return -1;
 }
 
 int deleteByKey(struct CLL *list, int key)
 {
-  int pos = searchByKey(list, key);
-  if (pos == -1)
-  {
-    printf("invalid pos to delete the key element\n");
-    return -1;
-  }
-  else
-  {
-    deleteAtPos(list, pos);
-  }
+      int pos = searchByKey(list, key);
+      if (pos == -1)
+      {
+            printf("invalid pos to delete the key element\n");
+            return -1;
+      }
+      else
+      {
+            deleteAtPos(list, pos);
+      }
 }
 
 void reverse(struct CLL *list)
 {
-  struct Node *current = list->head->next;
-  struct Node *nextNode = NULL;
-  struct Node *prev = NULL;
+      struct Node *current = list->head->next;
+      struct Node *nextNode = NULL;
+      struct Node *prev = NULL;
 
-  if (list->head->next == NULL)
-  {
-    printf("List is empty. Cannot reverse.\n");
-    return;
-  }
+      if (list->head->next == NULL)
+      {
+            printf("List is empty. Cannot reverse.\n");
+            return;
+      }
 
-  do
-  {
-    nextNode = current->next;
-    current->next = prev;
-    prev = current;
-    current = nextNode;
-  } while (current != list->head->next);
+      do
+      {
+            nextNode = current->next;
+            current->next = prev;
+            prev = current;
+            current = nextNode;
+      } while (current != list->head->next);
 
-  list->head->next = prev;
+      list->head->next = prev;
 }
 
 void copyList(struct CLL *list, struct CLL *newList)
 {
-  if (list->head->next == NULL)
-  {
-    printf("source list is empty.nothing to copy\n");
-    return;
-  }
-  struct Node *orginal = list->head->next;
-  do
-  {
-    insertAtEnd(newList, orginal->data);
-    orginal = orginal->next;
+      if (list->head->next == NULL)
+      {
+            printf("source list is empty.nothing to copy\n");
+            return;
+      }
+      struct Node *orginal = list->head->next;
+      do
+      {
+            insertAtEnd(newList, orginal->data);
+            orginal = orginal->next;
 
-  } while (orginal != list->head->next);
-  printf("\nlist copied -> ");
+      } while (orginal != list->head->next);
+      printf("\nlist copied -> ");
 }
 
 void insertInOrder(struct CLL *list, int value)
 {
-  struct Node *newNode = createNode(value);
+      struct Node *newNode = createNode(value);
 
-  if (list->head == NULL || value < list->head->next->data)
-  {
-    insertAtStart(list, value);
-    return;
-  }
+      if (list->head == NULL || value < list->head->next->data)
+      {
+            insertAtStart(list, value);
+            return;
+      }
 
-  struct Node *current = list->head->next;
-  struct Node *prev = NULL;
+      struct Node *current = list->head->next;
+      struct Node *prev = NULL;
 
-  while (value > current->data && current->next != list->head->next)
-  {
-    prev = current;
-    current = current->next;
-  }
-  if (value > current->data)
-  {
-    insertAtEnd(list, value);
-  }
-  else
-  {
-    prev->next = newNode;
-    newNode->next = current;
-    list->count++;
-  }
+      while (value > current->data && current->next != list->head->next)
+      {
+            prev = current;
+            current = current->next;
+      }
+      if (value > current->data)
+      {
+            insertAtEnd(list, value);
+      }
+      else
+      {
+            prev->next = newNode;
+            newNode->next = current;
+            list->count++;
+      }
 }
-
 
 int main()
 {
 
-  struct CLL *list = (struct CLL *)malloc(sizeof(struct CLL));
-  // struct CLL *newlist = (struct CLL *)malloc(sizeof(struct CLL));
-  createList(list, 10);
-  insertAtStart(list, 5);
-  insertAtEnd(list, 10);
-  insertAtEnd(list, 20);
-  insertAtEnd(list, 30);
-  insertAtStart(list, 2);
-  insertInOrder(list, 25);
-  display(list);
-  printf("\nElement at pos : %d",searchByPos(list,1));
-  return 0;
+      struct CLL *list = (struct CLL *)malloc(sizeof(struct CLL));
+      // struct CLL *newlist = (struct CLL *)malloc(sizeof(struct CLL));
+      createList(list, 10);
+      insertAtStart(list, 5);
+      insertAtEnd(list, 10);
+      insertAtEnd(list, 20);
+      insertAtEnd(list, 30);
+      insertAtStart(list, 2);
+      insertInOrder(list, 25);
+      display(list);
+      printf("\nElement at pos : %d", searchByPos(list, 1));
+      return 0;
 }
